7d1d26e51c7137d0629eb6479443824eb24baacd
Tung Nguyen <tung.nguyen.it0201@gmail.com>
2021-07-22 16:00:57 +0700

    style config

--- App/Themes/index.js
+++ App/Themes/index.js
@@ -2,3 +2,4 @@ export {default as Colors} from './Colors';
 export {default as Fonts} from './Fonts';
 export {default as Images} from './Images';
 export {default as Spacing} from './Spacing';
+export {default as Style} from './Style';
