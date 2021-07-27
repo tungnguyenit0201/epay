19bc1ef7f9ee32f2499ecf0e75d20550a03ccb51
nvtc.98 <nvtc.98@gmail.com>
2021-07-27 17:57:04 +0700

    setup image picker + refactor contexts

---
+++ App/Context/Wallet/index.js
@@ -0,0 +1,28 @@
+import React, {
+  createContext,
+  useState,
+  useContext,
+  useEffect,
+  useReducer,
+} from 'react';
+import userReducer from './reducer';
+import {useQuery} from 'react-query';
+import {getProfile} from 'services/user';
+
+const UserContext = createContext({});
+export const UserProvider = ({children}) => {
+  const {data} = useQuery('userInfo', getProfile, {staleTime: 10000});
+  console.log('data :>> ', data);
+  const [userInfo, dispatch] = React.useReducer(userReducer, data);
+
+  const value = React.useMemo(
+    () => ({
+      userInfo,
+    }),
+    [userInfo],
+  );
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
+
+export const useUser = () => useContext(UserContext);
