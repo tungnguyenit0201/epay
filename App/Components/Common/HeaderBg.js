ac28ec0632a06419df3640aa126f36d59de0a6d0
Quốc Toản <quoctoan@Apples-MacBook-Pro.local>
2021-08-06 17:01:04 +0700

    update home

---
+++ App/Components/Common/HeaderBg.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+import {View, Image, StyleSheet} from 'react-native';
+import {Colors, Images, Spacing, base} from 'themes';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {scale} from 'utils/Functions';
+
+const Header = ({children, style}) => {
+  const {top} = useSafeAreaInsets();
+  return (
+    <View style={[base.container, styles.header, {paddingTop: top + 5}, style]}>
+      <View style={styles.bg}>
+        <Image source={Images.Homes.BgHeader} style={styles.img} />
+      </View>
+      {children}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    paddingBottom: 20,
+    marginBottom: 20,
+  },
+  bg: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    overflow: 'hidden',
+    borderBottomRightRadius: 24,
+    borderBottomLeftRadius: 24,
+    backgroundColor: Colors.cl1,
+  },
+  img: {
+    width: scale(375),
+    height: '100%',
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    resizeMode: 'cover',
+  },
+});
+
+export default Header;
