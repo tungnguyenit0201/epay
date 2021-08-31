5c31ebdddc64a809c9e651973b4667302eef41e6
Tung Nguyen <tung.nguyen.it0201@gmail.com>
2021-08-31 15:17:01 +0700

    test eslint

--- App/Context/Common/utils/error.js
+++ App/Context/Common/utils/error.js
@@ -6,17 +6,20 @@ import {useCallback} from 'react';
 const useError = () => {
   const {dispatch} = useCommon();
   const translation = useTranslation();
-  const setError = useCallback(error => {
-    // let message = translation.errorCode[error?.ErrorCode];
-    dispatch({
-      type: 'SET_ERROR',
-      error: {
-        errorCode: error?.ErrorCode,
-        errorMessage: error?.ErrorMessage,
-        title: error?.title,
-      },
-    });
-  }, []); //eslint-disable-line
+  const setError = useCallback(
+    error => {
+      // let message = translation.errorCode[error?.ErrorCode];
+      dispatch({
+        type: 'SET_ERROR',
+        error: {
+          errorCode: error?.ErrorCode,
+          errorMessage: error?.ErrorMessage,
+          title: error?.title,
+        },
+      });
+    },
+    [dispatch],
+  );
   return {setError};
 };
 
