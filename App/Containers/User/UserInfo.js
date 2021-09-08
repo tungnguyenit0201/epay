9e2c40780588dd93180c2936d080d12bba98d98c
Loc <phuocloc17598@gmail.com>
2021-09-08 10:07:19 +0700

    fix comment

--- App/Containers/User/UserInfo.js
+++ App/Containers/User/UserInfo.js
@@ -20,7 +20,7 @@ import {usePhone} from 'context/Auth/utils';
 import {useTranslation} from 'context/Language';
 
 const UserInfo = () => {
-  //translate
+  // TODO: translate
   const {top} = useSafeAreaInsets();
   const {phone} = usePhone();
   const {userInfo} = useUser();
