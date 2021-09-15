566f4f3584c6edd1ec1fbb5810dfa20dc930d0db
hungpnh <phamnhuhuyhung@gmail.com>
2021-09-15 15:30:30 +0700

    test webpack with default

--- webpack.config.js
+++ webpack.config.js
@@ -31,15 +31,17 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015'],
-      }
+        test: /\.js$/,
+        exclude: /node_modules\/(?!()\/).*/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
       },{
         test: /\.css$/,
-        loader: 'style-loader!css-loader',
+        loader: "style-loader!css-loader"
       }, {
         test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
         loader: 'url-loader?limit=100000' 
