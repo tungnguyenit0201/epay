1ed0358150e8baf5c786740d0a44594593327fed
nvtc.98 <nvtc.98@gmail.com>
2021-09-15 16:09:33 +0700

    apply history list for History

--- App/Context/Wallet/utils/history.js
+++ App/Context/Wallet/utils/history.js
@@ -1,4 +1,4 @@
-import {ERROR_CODE, SCREEN} from 'configs/Constants';
+import {COMMON_ENUM, ERROR_CODE, SCREEN, TRANS_TYPE} from 'configs/Constants';
 import {useError} from 'context/Common/utils';
 import {useUser} from 'context/User';
 import Navigator from 'navigations/Navigator';
@@ -7,17 +7,70 @@ import {getHistory} from 'services/wallet';
 import _ from 'lodash';
 import moment from 'moment';
 
+const incomeType = [TRANS_TYPE.CashIn, TRANS_TYPE.CashReceive];
+const expenseType = [
+  TRANS_TYPE.CashOut,
+  TRANS_TYPE.CashTransfer,
+  TRANS_TYPE.AutoCashIn,
+  TRANS_TYPE.PaymentToll,
+  TRANS_TYPE.PaymentMerchant,
+];
+
 const useHistory = () => {
   const {phone} = useUser();
   const [historyData, setHistoryData] = useState([]);
   const {setError} = useError();
   const contentRef = useRef({
     search: '',
-    startDate: moment().format('DD-MM-YYYY hh:mm:ss'),
-    endDate: moment().subtract(1, 'months').format('DD-MM-YYYY hh:mm:ss'),
+    startDate: moment()
+      .subtract(1, 'years')
+      .format(COMMON_ENUM.DATETIME_FORMAT),
+    endDate: moment().format(COMMON_ENUM.DATETIME_FORMAT),
     serviceID: 0,
     stateID: 0,
   });
+
+  const parseHistory = data => {
+    // parser for grouping
+    const parser = item => {
+      const time = moment(item?.TransTime, COMMON_ENUM.DATETIME_FORMAT);
+      return `${time.month()}/${time.year()}`;
+    };
+    // group into object -> to array -> map into structured objects {key, list, income, expense}
+    // list is list from backend + field isIncome
+    // income, expense are the list filtered by transtype (income or expense) then calculate the total
+    const parsedData = Object.entries(_.groupBy(data, parser)).map(item => {
+      return {
+        key: item[0],
+        list: item[1].map(transaction => ({
+          ...transaction,
+          isIncome: incomeType.includes(transaction?.TransType),
+        })),
+        income: item[1]
+          .filter(transaction => incomeType.includes(transaction?.TransType))
+          .reduce(
+            (total, transaction) => parseInt(transaction?.TransAmount) + total,
+            0,
+          ),
+        expense: item[1]
+          .filter(transaction => expenseType.includes(transaction?.TransType))
+          .reduce(
+            (total, transaction) => parseInt(transaction?.TransAmount) + total,
+            0,
+          ),
+      };
+    });
+    // sort the array by month, latest months first
+    parsedData.sort((x, y) => {
+      const timeX = x.key.split('/');
+      const timeY = y.key.split('/');
+      if (parseInt(timeX[1]) === parseInt(timeY[1])) {
+        return parseInt(timeY[0]) - parseInt(timeX[0]);
+      }
+      return parseInt(timeY[1]) - parseInt(timeX[1]);
+    });
+    setHistoryData(parsedData);
+  };
 
   const onGetHistory = async () => {
     const {search, startDate, endDate, serviceID, stateID} = contentRef.current;
@@ -33,7 +86,7 @@ const useHistory = () => {
       setError(result);
       return;
     }
-    setHistoryData(result?.ListTransHist);
+    parseHistory(result?.ListTransHist);
   };
 
   useEffect(() => {
