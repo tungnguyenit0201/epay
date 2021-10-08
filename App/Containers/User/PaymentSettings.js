9e43fe757bf4581f60f25460ff9c8a3eeccaad9f
duynhut0405 <57499051+duynhut0405@users.noreply.github.com>
2021-10-08 11:05:43 +0700

    fix 1903

--- App/Containers/User/PaymentSettings.js
+++ App/Containers/User/PaymentSettings.js
@@ -22,81 +22,82 @@ const PaymentSettings = () => {
   const [xacNhan, isXacNhan] = useState(false);
   const {onGetLimit} = useUserInfo();
   return (
-    <ScrollView style={base.wrap}>
+    <>
       <HeaderBg>
         <Header back title={translation.payment_setting} />
       </HeaderBg>
+      <ScrollView style={base.wrap}>
+        <TouchableOpacity
+          style={styles.item}
+          onPress={() => {
+            // Navigator.navigate(SCREEN.AUTOPAYMENT);
+            Alert.alert('', 'Coming soon');
+          }}>
+          <Icon
+            mr={8}
+            icon={Images.Profile.MaThanhToan}
+            size={24}
+            tintColor={Colors.cl1}
+          />
+          <Text style={styles.text}> Cài đặt nạp tiền tự động</Text>
+          <Icon
+            style={[base.leftAuto]}
+            icon={Images.ArrowRight}
+            size={24}
+            tintColor="#000"
+          />
+        </TouchableOpacity>
+        <View style={styles.item}>
+          <Icon
+            mr={8}
+            icon={Images.Profile.MaThanhToan}
+            size={24}
+            tintColor={Colors.cl1}
+          />
+          <Text style={styles.text}> Xác nhận thanh toán nhanh</Text>
+          <Switch
+            style={base.leftAuto}
+            onColor={Colors.cl1}
+            offColor={Colors.l3}
+            value={xacNhan}
+            onValueChange={isXacNhan}
+          />
+        </View>
+        <Pressable style={styles.item} onPress={onGetLimit}>
+          <Icon
+            mr={8}
+            icon={Images.Profile.MaThanhToan}
+            size={24}
+            tintColor={Colors.cl1}
+          />
+          <Text style={styles.text}> Hạn mức trong ngày</Text>
+        </Pressable>
+        <TouchableOpacity
+          style={styles.item}
+          onPress={() => {
+            // Navigator.navigate(SCREEN.NOTIFICATION);
+            Alert.alert('', 'Coming soon');
+          }}>
+          <Icon
+            mr={8}
+            icon={Images.Profile.MaThanhToan}
+            size={24}
+            tintColor={Colors.cl1}
+          />
+          <Text style={styles.text}> Đăng ký thanh toán giao thông</Text>
+          <Icon
+            style={[base.leftAuto]}
+            icon={Images.ArrowRight}
+            size={24}
+            tintColor="#000"
+          />
+        </TouchableOpacity>
 
-      <TouchableOpacity
-        style={styles.item}
-        onPress={() => {
-          // Navigator.navigate(SCREEN.AUTOPAYMENT);
-          Alert.alert('', 'Coming soon');
-        }}>
-        <Icon
-          mr={8}
-          icon={Images.Profile.MaThanhToan}
-          size={24}
-          tintColor={Colors.cl1}
-        />
-        <Text style={styles.text}> Cài đặt nạp tiền tự động</Text>
-        <Icon
-          style={[base.leftAuto]}
-          icon={Images.ArrowRight}
-          size={24}
-          tintColor="#000"
-        />
-      </TouchableOpacity>
-      <View style={styles.item}>
-        <Icon
-          mr={8}
-          icon={Images.Profile.MaThanhToan}
-          size={24}
-          tintColor={Colors.cl1}
-        />
-        <Text style={styles.text}> Xác nhận thanh toán nhanh</Text>
-        <Switch
-          style={base.leftAuto}
-          onColor={Colors.cl1}
-          offColor={Colors.l3}
-          value={xacNhan}
-          onValueChange={isXacNhan}
-        />
-      </View>
-      <Pressable style={styles.item} onPress={onGetLimit}>
-        <Icon
-          mr={8}
-          icon={Images.Profile.MaThanhToan}
-          size={24}
-          tintColor={Colors.cl1}
-        />
-        <Text style={styles.text}> Hạn mức trong ngày</Text>
-      </Pressable>
-      <TouchableOpacity
-        style={styles.item}
-        onPress={() => {
-          // Navigator.navigate(SCREEN.NOTIFICATION);
-          Alert.alert('', 'Coming soon');
-        }}>
-        <Icon
-          mr={8}
-          icon={Images.Profile.MaThanhToan}
-          size={24}
-          tintColor={Colors.cl1}
-        />
-        <Text style={styles.text}> Đăng ký thanh toán giao thông</Text>
-        <Icon
-          style={[base.leftAuto]}
-          icon={Images.ArrowRight}
-          size={24}
-          tintColor="#000"
-        />
-      </TouchableOpacity>
-
-      <View style={[base.container]}>
-        <Text>{`Cài đặt hạn mức: ${formatMoney(50000000)}đ`}</Text>
-      </View>
-    </ScrollView>
+        <View style={[base.container]}>
+          <Text>{`Cài đặt hạn mức: ${formatMoney(50000000)}đ`}</Text>
+        </View>
+      </ScrollView>
+    </>
   );
 };
 const styles = StyleSheet.create({
