e6d567dda1c88cb0c7930a9ec27c6d6d5fb15286
duynhut0405 <57499051+duynhut0405@users.noreply.github.com>
2021-10-11 12:03:15 +0700

    fix 1901

--- App/Containers/User/SmartOTP/ActiveSmartOTP.js
+++ App/Containers/User/SmartOTP/ActiveSmartOTP.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {ScrollView, StyleSheet, View} from 'react-native';
+import {ScrollView, StyleSheet, View, Image} from 'react-native';
 import {Text, Button, Icon, Header, Radio, HeaderBg} from 'components';
 import {base} from 'themes';
 import {useTranslation} from 'context/Language';
 import {useSmartOTP} from 'context/User/utils';
 import FooterContainer from 'components/Auth/FooterContainer';
+import {scale} from 'utils/Functions';
 const ActiveSmartOtp = () => {
   const translation = useTranslation();
   const {phone, isAccepted, onAcceptTermConditions, onGoOTP} = useSmartOTP();
@@ -30,6 +31,7 @@ const ActiveSmartOtp = () => {
             <Text bold>Lưu ý: </Text>Smart OTP trên thiết bị khác sẽ bị vô hiệu
             hóa sau khi kích hoạt thành công ở thiết bị này
           </Text>
+          <Image source={require('images/wave.png')} style={styles.bgImg} />
         </View>
       </ScrollView>
       <FooterContainer>
@@ -59,5 +61,10 @@ const ActiveSmartOtp = () => {
     </>
   );
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  bgImg: {
+    width: scale(375),
+    height: scale(375),
+  },
+});
 export default ActiveSmartOtp;
