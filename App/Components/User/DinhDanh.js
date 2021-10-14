2e7d95550b2265eda9a35001deada427bb12c12a
hoangtho123456 <danghoangtho1994@gmail.com>
2021-10-14 12:42:45 +0700

    fixing layout userInfo

--- App/Components/User/DinhDanh.js
+++ App/Components/User/DinhDanh.js
@@ -5,6 +5,7 @@ import {Colors, base} from 'themes';
 import {SCREEN} from 'configs/Constants';
 import Navigator from 'navigations/Navigator';
 import {useTranslation} from 'context/Language';
+import autoMergeLevel1 from 'redux-persist/es/stateReconciler/autoMergeLevel1';
 const DinhDanh = () => {
   //TODO : translation
   const translation = useTranslation();
@@ -16,10 +17,15 @@ const DinhDanh = () => {
           source={require('images/profile/User.png')}
         />
       </View>
-      <Text fs="h4" centered bold mb={10}>
+      <Text fs="h6" centered bold mb={8}>
         {translation.verify_your_account}
       </Text>
-      <Text centered mb={20}>
+      <Text
+        centered
+        mb={20}
+        fs="md"
+        style={[styles.maxWidth1, styles.marginCenter]}
+      >
         {
           translation.verified_account_will_have_a_higher_level_of_security_and_greater_limits_for_transactions
         }
@@ -30,15 +36,28 @@ const DinhDanh = () => {
         onPress={() => {
           Navigator.navigate(SCREEN.CHOOSE_IDENTITY_CARD);
         }}
+        size="sm"
+        style={[styles.maxWidth2, styles.marginCenter]}
       />
     </View>
   );
 };
 const styles = StyleSheet.create({
+  marginCenter: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  //----------------
   ico: {
     flexDirection: 'row',
     justifyContent: 'center',
     marginBottom: 10,
+  },
+  maxWidth1: {
+    maxWidth: 311,
+  },
+  maxWidth2: {
+    maxWidth: 240,
   },
 });
 export default DinhDanh;
