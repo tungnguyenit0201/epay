27f25c111d41b4f61209ecbc13f5af8978643124
Tung Nguyen <tung.nguyen.it0201@gmail.com>
2022-02-09 14:17:04 +0700

    test build app center

--- ios/epay/AppDelegate.m
+++ ios/epay/AppDelegate.m
@@ -41,9 +41,9 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [AppCenterReactNative register];
-  [AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
-  [AppCenterReactNativeCrashes registerWithAutomaticProcessing];
+[AppCenterReactNative register];
+[AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
+[AppCenterReactNativeCrashes registerWithAutomaticProcessing];
 
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
